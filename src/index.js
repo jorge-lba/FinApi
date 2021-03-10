910092efb536c920ccfdeb1810ed757ea7c785d9
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-03-09 21:39:14 -0300

    Add - Middlewares

--- src/index.js
+++ src/index.js
@@ -7,6 +7,19 @@ app.use(express.json())
 
 const customers = []
 
+function verifyExistsAccountCPF(request, response, next) {
+    const { cpf } = request.headers
+    
+    const customer = customers.find(customer => customer.cpf === cpf)
+
+    if(!customer) {
+        return response.status(400).json({error: "Customer not found"})
+    }
+    request.customer = customer
+
+    next()
+}
+
 app.post('/account', (request, response) => {
     const { cpf, name } = request.body
 
@@ -30,15 +43,8 @@ app.post('/account', (request, response) => {
     return response.status(201).send()
 })
 
-app.get('/statement', (request, response) => {
-    const { cpf } = request.headers
-    
-    const customer = customers.find(customer => customer.cpf === cpf)
-
-    if(!customer) {
-        return response.status(400).json({error: "Customer not found"})
-    }
-
+app.get('/statement', verifyExistsAccountCPF, (request, response) => {   
+    const { customer } = request
     return response.json(customer.statement)
 })
 
