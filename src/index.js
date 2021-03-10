caee70ee9f1f166ba00107bd1a3d39c6f3005a56
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-03-09 22:00:29 -0300

    Add - Rota para saques

--- src/index.js
+++ src/index.js
@@ -20,6 +20,16 @@ function verifyExistsAccountCPF(request, response, next) {
     next()
 }
 
+function getBalance(statement){
+    const balance = statement.reduce((acc, operation) => {
+        return operation.type === 'credit'
+            ? acc + operation.amount
+            : acc - operation.amount
+    }, 0)
+
+    return balance
+}
+
 app.post('/account', (request, response) => {
     const { cpf, name } = request.body
 
@@ -58,6 +68,27 @@ app.post('/deposit', verifyExistsAccountCPF, (request, response) => {
         amount,
         created_at: new Date(),
         type: 'credit'
+    }
+
+    customer.statement.push(statementOperation)
+
+    return response.status(201).send()
+})
+
+app.post('/withdraw', verifyExistsAccountCPF, (request, response) => {
+    const { amount } = request.body
+    const { customer } = request
+
+    const balance = getBalance(customer.statement)
+
+    if(balance < amount){
+        return response.status(400).json({error: 'Insufficient funds!'})
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: 'debit'
     }
 
     customer.statement.push(statementOperation)
